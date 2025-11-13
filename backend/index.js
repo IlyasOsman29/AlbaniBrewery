463875af0eed6426f9bb5cb66108a1b67c21fb19
Ilyas <ilyas02904@gmail.com>
2025-11-13 04:22:36 +0100

    Update index.js
    Index.js loades routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 // index.js
 console.log("Hej fra Node.js!");
 
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+
+app.use(express.json());
+
+// Load routes
+routes(app);
+
+app.listen(3000, () => console.log("Backend running on port 3000"));
